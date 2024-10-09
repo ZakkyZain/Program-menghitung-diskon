7341e8a6661112d7f66b80955f276e748aae87c1
unknown <24051214209@mhs.unesa.ac.id>
2024-10-09 14:47:25 +0700

    first commit

---
+++ Python 1.py
@@ -0,0 +1,19 @@
+print('program phython diskon potongan harga')
+print('=====================================')
+print()
+
+total_belanja = int(input('Total Belanja: Rp.'))
+
+if (total_belanja >=100000) and (total_belanja < 500000):
+    harga_akhir = total_belanja - (0.1*total_belanja)
+    print('Selamat, anda mendapat diskon 10%')
+elif (total_belanja >= 500000) and (total_belanja < 1000000):
+    harga_akhir = total_belanja - (0.2*total_belanja)
+    print('Selamat, anda mendapat diskon 20%')
+elif (total_belanja >= 1000000):
+    harga_akhir = total_belanja - (0.35*total_belanja)
+    print('Selamat, anda mendapat diskon 35%')
+else:
+    harga_akhir = total_belanja
+    
+print('Total bayar: Rp.',round(harga_akhir,2))
